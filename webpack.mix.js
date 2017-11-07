be56b9bd057f64f2b310390566a00387f34ce781
vxuandai0212 <vxuandai@gmail.com>
2017-11-07 16:18:00 +0700

    update sass folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,4 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .sass('resources/assets/sass/style.scss', 'public/css/style.css');
+   .sass('resources/assets/sass/about/about.scss', 'public/css/about/about.css');
